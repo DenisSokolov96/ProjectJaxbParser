659c9006879ca220320cea2b03f5d033be3c9a22
DenisSokolov96 <dens111296@gmail.com>
2020-01-22 21:04:39 +0300

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,40 @@
+import org.apache.log4j.Logger;
+import javax.xml.bind.*;
+import java.io.File;
+import ru.rsatu.denis.*;
+
+
+public class MainClass {
+
+    public static final Logger log = Logger.getLogger(MainClass.class);
+    public static StringBuilder str = new StringBuilder();
+
+    public static void main(String[] args){
+
+        try {
+
+            str.append("Read XML"+"\n");
+            JAXBContext context = JAXBContext.newInstance(UniversityType.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            String path = "/home/denis/IntelliJIDEAProjects/ProjectJaxbParser/src/main/resources/Univer.xml";
+            JAXBElement<UniversityType> univer = (JAXBElement<UniversityType>) unmarshaller.unmarshal(new File(path));
+            str.append(univer +"\n");
+            str.append("\n***************\n");
+
+
+
+            str.append("Write XML"+"\n");
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            path = "/home/denis/IntelliJIDEAProjects/ProjectJaxbParser/src/main/resources/Output.xml";
+            marshaller.marshal(univer, new File(path));
+            str.append("Creat XML: "+path+"\n");
+            log.info("Ok");
+
+        }catch (JAXBException e){
+            log.error(e.getMessage());
+            str.append("Error");
+        }
+        System.out.println(str);
+    }
+}
